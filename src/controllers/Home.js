31b5dcf66e2d8410b282e6192b3dfc3d478527b8
odairsp <odair.sp@hotmail.com>
2023-08-16 14:35:40 -0300

    att

--- src/controllers/Home.js
+++ src/controllers/Home.js
@@ -1,3 +1,5 @@
+const { MAIN_DIR } = require("../helpers/constants");
+
 const index = function (request, response) {
   return response.render("home", { layout: MAIN_DIR, title: "Home@index" });
 };
--- src/helpers/handlebars.js
+++ src/helpers/handlebars.js
@@ -1,14 +1,10 @@
-const exphbs = require("express-handlebars");
+const { create } = require("express-handlebars");
 const { PARTIALS_DIR, LAYOUTS_DIR, VIEWS_DIR } = require("./constants");
 
+const engine = create({ partialsDir: PARTIALS_DIR, layoutsDir: LAYOUTS_DIR });
+
 exports.init = function (app) {
-  app.engine(
-    "html",
-    exphbs({
-      partialsDir: PARTIALS_DIR,
-      layoutsDir: LAYOUTS_DIR,
-    })
-  );
+  app.engine("html", engine.engine);
 
   app.set("view engine", "html");
   app.set("views", VIEWS_DIR);
